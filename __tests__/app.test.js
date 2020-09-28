dd8f9aad3dee697a2371d9003d0818b1c904d035
internetcharles <charlesodellsmith@gmail.com>
2020-09-28 15:45:24 -0700

    verify

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,44 +8,71 @@ describe('tardygram routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
-});
 
-it('signup a user via POST', async () => {
-  const response = await request(app)
-    .post('/api/v1/auth/signup')
-    .send({
-      email: 'email@email.com',
-      password: '1234',
-      profilePhotoUrl: 'a.jpeg'
-    });
+  it('signup a user via POST', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'email@email.com',
+        password: '1234',
+        profilePhotoUrl: 'a.jpeg'
+      });
 
-    expect(response.body).toEqual({
-      id: expect.any(String),
-      email: 'email@email.com',
-      profile_photo_url: 'a.jpeg'
+      expect(response.body).toEqual({
+        id: expect.any(String),
+        email: 'email@email.com',
+        profile_photo_url: 'a.jpeg'
 
-    });
-});
-
-it('logs in a user via POST', async() => {
-  const user = await UserService.create({
-    email: '1234@123.com',
-    password: '123',
-    profilePhotoUrl: 'a.jpeg'
+      });
   });
 
-  const response = await request(app)
-    .post('/api/v1/auth/login')
-    .send({
+  it('logs in a user via POST', async() => {
+    const user = await UserService.create({
       email: '1234@123.com',
       password: '123',
       profilePhotoUrl: 'a.jpeg'
     });
 
-    expect(response.body).toEqual({
-      id: user.id,
-      email: '1234@123.com',
-      profile_photo_url: 'a.jpeg'
-    })
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: '1234@123.com',
+        password: '123',
+        profilePhotoUrl: 'a.jpeg'
+      });
 
-})
+      expect(response.body).toEqual({
+        id: user.id,
+        email: '1234@123.com',
+        profile_photo_url: 'a.jpeg'
+      })
+  });
+
+  it('verifies a user via GET', async() => {
+    const agent = request.agent(app);
+    await agent
+      .post('/api/v1/auth/signup')
+      .send({
+        email: '1234@123.com',
+        password: '1234',
+        profilePhotoUrl: 'a.jpeg'
+      });
+
+      const response = await agent
+        .get('/api/v1/auth/verify');
+        
+      expect(response.body).toEqual({
+        id: expect.any(String),
+        email: '1234@123.com'
+      });
+
+      const responseWithoutAUser = await request(app)
+        .get('/api/v1/auth/verify');
+
+      expect(responseWithoutAUser.body).toEqual({
+        status: 500,
+        message: 'jwt must be provided'
+      });
+  })
+
+});
