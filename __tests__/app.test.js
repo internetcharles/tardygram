f6eab42ccc54a952e3ce7140c31f59813067c80f
internetcharles <charlesodellsmith@gmail.com>
2020-09-28 14:28:33 -0700

    first test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/user-service');
 
 describe('tardygram routes', () => {
   beforeEach(() => {
@@ -9,6 +10,42 @@ describe('tardygram routes', () => {
   });
 });
 
-it('signup a user via POST', () => {
+it('signup a user via POST', async () => {
   const response = await request(app)
+    .post('/api/v1/auth/signup')
+    .send({
+      email: 'email@email.com',
+      password: '1234',
+      profilePhotoUrl: 'a.jpeg'
+    });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'email@email.com',
+      profile_photo_url: 'a.jpeg'
+
+    });
+});
+
+it('logs in a user via POST', async() => {
+  const user = await UserService.create({
+    email: '123@123.com',
+    password: '123',
+    profile_photo_url: 'a.jpeg'
+  });
+
+  const response = await request(app)
+    .post('/api/v1/auth/login')
+    .send({
+      email: '123@123.com',
+      password: '123',
+      profile_photo_url: 'a.jpeg'
+    });
+
+    expect(response.body).toEqual({
+      id: user.id,
+      email: '123@123.com',
+      profile_photo_url: 'a.jpeg'
+    })
+
 })
